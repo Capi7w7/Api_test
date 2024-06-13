8fe80a37f4a1f5173bcc9fbb57dcc9edf43886cd
Felipe <fe.ferradap@duocuc.cl>
2024-06-12 22:39:01 -0400

    Intalacion de cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+import cors from "cors": 5k (gzipped: 2.1k)
+
 const express = require('express');
 const { getVehiculo, getVehiculoById } = require('./controllers/vehiculos.controller');
+
 const app = express();
 const port = 3001;
 
-
+app.use(cors());
 app.get("/", getVehiculo);
 
 app.get("/:id", getVehiculoById);
